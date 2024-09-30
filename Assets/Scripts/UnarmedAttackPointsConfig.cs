596f3dea6593d336b4102596e28e0a24d41dd116
weizhengliang <lwz12@outlook.com>
2024-09-30 11:22:24 +0800

    feat: Added ScriptableObject type specifically for store Unarmed AttackPoints data for each one character

---
+++ Assets/Scripts/UnarmedAttackPointsConfig.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnarmedAttackPointsConfig", menuName = "RPG/Unarmed Attack Points Config")]
+public class UnarmedAttackPointsConfig : ScriptableObject
+{
+    [System.Serializable]
+    public class UnarmedAttackPoint
+    {
+        public string name;
+        public string boneName;
+        public Vector3 localPosition;
+    }
+
+    public List<UnarmedAttackPoint> attackPoints = new List<UnarmedAttackPoint>();
+}
